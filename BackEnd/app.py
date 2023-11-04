d373b61d3fc3c136581b2022fb8a381f879133f7
yoinqui <yoann.inquimbert@viacesi.fr>
2023-11-04 20:05:42 +0100

    BackEnd Python CRUD Evol 1 Part 1

---
+++ BackEnd/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    # id = db.Column(db.)
